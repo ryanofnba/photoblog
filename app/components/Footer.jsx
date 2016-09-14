f490c24654b515d4d6e7eb16f37e39dc2511c6ab
Ryan Huang <ryan_huang@intuit.com>
2016-09-14 13:51:04 -0700

    responsive menu

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,13 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <div>
-          <IndexLink
-            className="footer-text"
-            to="/"
-            activeClassName="active"
-            activeStyle={{fontWeight: 'bold', color: '#757575'}}>HOME</IndexLink>
-        </div>
+
       </div>
     );
   }
