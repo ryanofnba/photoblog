a38ef41e9af70fa02a246b79bed8f406eae8943b
Ryan Huang <ryan_huang@intuit.com>
2016-09-14 12:14:19 -0700

    remove firebase

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,18 +1,18 @@
-import firebase from 'firebase';
-
-try {
-  const config = {
-      apiKey: process.env.API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      databaseURL: process.env.DATABASE_URL,
-      storageBucket: process.env.STORAGE_BUCKET,
-    };
-
-  firebase.initializeApp(config);
-} catch (e) {
-
-}
-
-export const facebookProvider = new firebase.auth.FacebookAuthProvider();
-export const firebaseRef = firebase.database().ref();
-export default firebase;
+// import firebase from 'firebase';
+//
+// try {
+//   const config = {
+//       apiKey: process.env.API_KEY,
+//       authDomain: process.env.AUTH_DOMAIN,
+//       databaseURL: process.env.DATABASE_URL,
+//       storageBucket: process.env.STORAGE_BUCKET,
+//     };
+//
+//   firebase.initializeApp(config);
+// } catch (e) {
+//
+// }
+//
+// export const facebookProvider = new firebase.auth.FacebookAuthProvider();
+// export const firebaseRef = firebase.database().ref();
+// export default firebase;
